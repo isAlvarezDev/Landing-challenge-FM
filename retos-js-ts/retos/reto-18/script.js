5f8e5fc861178271e09c7ab7b95f16377bda2072
JuSeRDev <juserdev@gmail.com>
2024-12-19 12:14:35 -0500

    atrapo los valores de los input en una variable para cada uno

--- retos-js-ts/retos/reto-18/script.js
+++ retos-js-ts/retos/reto-18/script.js
@@ -3,17 +3,27 @@ var challenge18;
 (function (challenge18) {
     const regEx = /[^0-9.]/g;
     const porcentage = document.querySelectorAll(".porcentage-number");
-    let inputBill = document.querySelector(".input-bill");
-    let inputNumberPeople = document.querySelector(".number-people");
+    const inputBill = document.querySelector(".input-bill");
+    const inputNumberPeople = document.querySelector(".number-people");
+    const reset = document.querySelector(".reset");
     let clickedText;
-    let addInputBill;
-    const applyNumberFilter = (param) => {
-        param.addEventListener("input", () => {
-            param.value = param.value.replace(regEx, "");
-        });
-    };
-    applyNumberFilter(inputBill);
-    applyNumberFilter(inputNumberPeople);
+    let addInputBill = 0;
+    let addInpuNumberPeople = 0;
+    inputBill.addEventListener("input", () => {
+        inputBill.value = inputBill.value.replace(regEx, "");
+        addInputBill = Number(inputBill.value);
+        console.log(addInputBill);
+    });
+    inputNumberPeople.addEventListener("input", () => {
+        inputNumberPeople.value = inputNumberPeople.value.replace(regEx, "");
+        addInpuNumberPeople = Number(inputNumberPeople.value);
+        console.log(addInpuNumberPeople);
+    });
+    // este es un boton de prueba
+    reset.addEventListener("click", () => {
+        console.log("Este es el valor del dinero: ", addInputBill);
+        console.log("Esta es la cantidad de personas: ", addInpuNumberPeople);
+    });
     const clicked = () => {
         porcentage.forEach(element => {
             element.addEventListener("click", () => {
@@ -22,7 +32,6 @@ var challenge18;
                 element.classList.toggle("porcentage-number-active");
                 clickedText = Number((_a = element.textContent) === null || _a === void 0 ? void 0 : _a.replace("%", "")); // Number() combierte un string en Number (La inicial debe estar en mayuscula)
                 console.log(clickedText);
-                // voy a hacer funcionar el regex aqui
             });
         });
     };
--- retos-js-ts/retos/reto-18/script.ts
+++ retos-js-ts/retos/reto-18/script.ts
@@ -3,20 +3,33 @@ namespace challenge18 {
     const regEx = /[^0-9.]/g;
     
     const porcentage = document.querySelectorAll(".porcentage-number") as NodeListOf<HTMLParagraphElement>
-    let inputBill = document.querySelector(".input-bill") as HTMLInputElement
-    let inputNumberPeople = document.querySelector(".number-people") as HTMLInputElement
+    const inputBill = document.querySelector(".input-bill") as HTMLInputElement
+    const inputNumberPeople = document.querySelector(".number-people") as HTMLInputElement
+    const reset = document.querySelector(".reset") as HTMLParagraphElement
     
     let clickedText: number
-    let addInputBill: number 
     
-    const applyNumberFilter = (param: HTMLInputElement): void =>{
-        param.addEventListener("input", ()=>{
-            param.value = param.value.replace(regEx, "")
-        })
-    }
+    let addInputBill: number = 0
+    let addInpuNumberPeople: number = 0
 
-    applyNumberFilter(inputBill)
-    applyNumberFilter(inputNumberPeople)
+    inputBill.addEventListener("input", ()=>{
+        inputBill.value = inputBill.value.replace(regEx, "")
+        addInputBill = Number(inputBill.value)
+        console.log(addInputBill)
+    })
+
+    inputNumberPeople.addEventListener("input", ()=>{
+        inputNumberPeople.value = inputNumberPeople.value.replace(regEx, "")
+        addInpuNumberPeople = Number(inputNumberPeople.value)
+        console.log(addInpuNumberPeople)
+    })
+
+
+    // este es un boton de prueba
+    reset.addEventListener("click", ()=>{
+        console.log("Este es el valor del dinero: ",addInputBill)
+        console.log("Esta es la cantidad de personas: ", addInpuNumberPeople)
+    })
 
 
     const clicked = () =>{
@@ -28,10 +41,9 @@ namespace challenge18 {
                 clickedText = Number(element.textContent?.replace("%", "")) // Number() combierte un string en Number (La inicial debe estar en mayuscula)
                 console.log(clickedText)
 
-                // voy a hacer funcionar el regex aqui
-
             })
         });
+        
     }
 
 
